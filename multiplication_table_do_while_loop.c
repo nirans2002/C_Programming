f1229347afd91760b5495a5d1d54c4d4b5010164
nirans2002 <nirans2002@gmail.com>
2021-05-25 09:43:39 +0530

    lab 3

---
+++ multiplication_table_do_while_loop.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+    int i=0,j=0,n=0,m=0;
+    printf("\n Program for multiplication table n x m \n ");
+    printf("Enter value for n : ");
+    scanf("%d",&n);
+    printf("Enter value for m : ");
+    scanf("%d",&m);
+
+    for(i=1;i<=n;i++)
+    {
+        printf("MUltiplication table of %d x %d:\n",n,m);
+        for(j=1;j<=m;j++)
+        {
+            printf("%d x %d = %d \n",i,j,i*j);
+        }
+    }
+}
