7c7a430b48f054f00e374dae72da1633b244e356
Niran S Narayanan <nirans2002@gmail.com>
2021-08-01 16:05:37 +0530

    updated

--- AngstromNo.c
+++ AngstromNo.c
@@ -1,2 +1,27 @@
 #include<stdio.h>
 #include<conio.h>
+
+int main()
+{
+    int n,a,r,sum=0;
+    printf("\n\n Enter a number: ");
+    scanf("%d",&n);
+    a=n;
+
+    while (n>0)
+    {
+        r = n%10;
+        sum = sum + (r*r*r);
+        n = n/10;
+    }
+    if(sum == a)
+    {
+        printf("\n\n  The number %d is an Angstrong Number.",a);
+    }
+    else 
+    {
+        printf("\n\n  The number %d is NOT an Angstrong Number.",a);
+    
+    }
+    
+}
