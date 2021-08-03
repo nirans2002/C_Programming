2c9a37ef073bee39e586b7a20c58225a8538bb07
Niran S Narayanan <nirans2002@gmail.com>
2021-08-03 10:20:18 +0530

    lab 1

---
+++ LAB/5-Convert-an-integer-to-a-character.c
@@ -0,0 +1,24 @@
+// Aim: To convert an integer to a character
+
+// Algorithm
+// * Declare a variable as type integer
+// * Read its value through the keyboard
+// * The entered number and ASCII value are checked
+// * If there is a match, display the ASCII value
+
+
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+    int n;
+    char c;
+    printf("\n\n  Enter an integer: ");
+    scanf("%d",&n);
+
+    c = n;
+    printf("\n\n  The integer in %d as character is %c ",n,c);
+
+}
+
