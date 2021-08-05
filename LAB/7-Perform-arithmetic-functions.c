a15319d50b5f1ed29bc1e385e7a08cdd91f6e885
Niran S Narayanan <nirans2002@gmail.com>
2021-08-05 16:54:09 +0530

    files added

---
+++ LAB/7-Perform-arithmetic-functions.c
@@ -0,0 +1,61 @@
+
+// Aim:To perform various arithmetic functions such as Addition, Subtraction, Multiplication, Division, Remainder Calculation etc.
+
+
+// Algorithm:
+// * Display a menu with 5 options and request the user to enter the choice
+// * Go to suitable sections based on the entered choice
+// * Prompt the user to enter two numbers
+// * Perform the relevant operation
+// * Display the result
+// * Warn the user with “Invalid Choice”, if the entered choice is wrong
+
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int operator=0;
+    float a,b,result;
+    printf("\n Choose an Operation \n"
+            "\t 1) Addition \n "
+            "\t 2) Subtraction \n"
+            "\t 3) Multiplication \n"
+            "\t 4) Division \n"
+            "\t 5) Remainder Calculation \n");
+    printf("Enter the option number : ");
+    scanf("%d",&operator);
+    printf("\nEnter the first Operand : ");
+    scanf("%f",&a);
+    printf("\nEnter the second Operand : ");
+    scanf("%f",&b);
+    printf("\n");
+
+    switch (operator)
+    {
+        case 1:
+            result = a+b;
+            printf("  %f + %f = %f",a,b,result);
+            break;
+        case 2:
+            result = a-b;
+            printf("  %f - %f = %f",a,b,result);
+            break;
+        case  3:
+            result = a*b;
+            printf("  %f * %f = %f",a,b,result);
+            break;
+        case 4:
+            result = a / b;
+            printf("  %f / %f = %f",a,b,result);
+            break;
+        case 5:
+            result = (int)a % (int)b;
+            printf("  %f // %f = %d",a,b,(int)result);
+            break;
+        default:
+            printf("Error!! invalid choice.");
+}
+    return 0;
+}
---
+++ LAB/8-Check-a-number-for-prime-or-not.c
@@ -0,0 +1,43 @@
+// Aim:To check whether the given number is prime or not
+
+
+// Algorithm:
+// Enter the number n
+// Perform the ‘mod’ operation from 2 to (n-1).
+// If the remainder is zero, ‘n’ is not a prime number.
+// If remainder is non-zero, then ‘n’ is a prime number
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int n=0,i,flag=0;
+    printf("\n Enter an integer: ");
+    scanf("%d",&n);
+    for (i=2;i<=(n-1);i++)
+    {
+        if (n==1)
+        {
+            printf(" 1 is niether a prime nor a composite number");
+        }
+
+        if (n%i == 0)
+        {
+            flag = 1;
+            break;
+        }
+        else
+        {
+            if (flag == 0)
+            {
+                printf("the number %d is a prime number",&n);
+            }
+            else
+            {
+                printf("the number %d is NOT a prime number",&n);
+            }
+        }
+    }
+    return 0;
+}
