062ffb28961c2058aa69769d3327758a6d2ce1bb
Niran S Narayanan <nirans2002@gmail.com>
2021-09-03 15:17:56 +0530

    14

---
+++ LAB/14-compare-two-strings.c
@@ -0,0 +1,42 @@
+// Aim: To compare two strings without any standard function and if the strings are
+// not identical, display the position where the characters are different
+// Algorithm
+//  Read in the two strings
+//  Using a loop, check the corresponding characters of both the strings
+//  If identical, loop is continued
+//  Otherwise, a counter variable ‘diff’ is incremented
+//  Display the locations where the strings are non identical
+//  Display the no: of places, where two strings are different
+
+
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+void main() {
+    char string1[50];
+    char string2[50];
+    int diff = 0;
+    int flag = 0; 
+
+
+    printf("Enter the first string\n");
+    scanf("%s",string1);
+    printf("Enter the second string\n");
+    // gets(string2);
+    scanf("%s",string2);
+
+
+    for (int i = 0; string1[i] != '\0'; i++) {
+        if (string1[i] != string2[i]){
+            flag = 1;
+            diff = i;
+            printf("the strings differ at positon %d\n",diff+1);
+            
+        }
+    }
+
+    if (flag == 0){
+        printf("the strings are identical");
+    }
+}
---
+++ LAB/15-concatenate-two-strings.c
@@ -0,0 +1,29 @@
+// Aim: To concatenate two strings without a standard function
+// Algorithm
+//  Read in the two strings
+//  Declare a 3rd string to store the concatenated string
+//  Use a loop, store the 1st string to the third
+//  Store the 2nd string to the 3rd, starting from the current position
+//  Display the third string
+
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+void main()
+{
+    char string1[100];
+    char string2[100];
+    char string3[100];
+
+
+    printf("Enter the first string\n");
+    gets(string1);
+    printf("Enter the second string\n");
+    gets(string2);
+
+     for (int i = 0; string1[i] != '\0'; i++)
+      {
+
+    }
+}
